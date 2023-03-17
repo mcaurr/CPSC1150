35ab67ca3712f482b531e0e362ca4db9c37bf457
mcaurr <128012056+mcaurr@users.noreply.github.com>
2023-03-16 17:53:25 -0700

    Update Lab1.java

--- Labs/Lab1.java
+++ Labs/Lab1.java
@@ -1,7 +1,20 @@
 package Labs;
-
+import java.util.Scanner;
 public class Lab1 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        double radius;
+        int length;
+        double volume;
+        double area;
+
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the radius of the cylinder: ");
+        radius = sc.nextDouble();
+        System.out.print("Enter the length of the cylinder: ");
+        length = sc.nextInt();
+        area = ((radius * radius) * Math.PI);
+        volume = (area * length);
+        System.out.println(volume);
+        System.out.format("%.1f", volume );
     }
 }
