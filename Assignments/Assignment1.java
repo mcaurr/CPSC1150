0df9d313dbfcc15009b95abd226ff9d9e0b3ff21
mcaurr <128012056+mcaurr@users.noreply.github.com>
2023-03-16 17:35:15 -0700

    Lab0 - Printing my Information

---
+++ Assignments/Assignment1.java
@@ -0,0 +1,6 @@
+package Assignments;
+public class Assignment1{
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ Labs/Lab0.java
@@ -0,0 +1,13 @@
+package Labs;
+
+public class Lab0 {
+    public static void main(String[] args) {
+        System.out.println("****************************************");
+        System.out.println("*                                      *");
+        System.out.println("*    Name: Manpreet Kaur               *");
+        System.out.println("*    Student number: 100               *");
+        System.out.println("*    Courses: 1150, 1271, 1200         *");
+        System.out.println("*                                      *");
+        System.out.println("****************************************");
+    }
+}
